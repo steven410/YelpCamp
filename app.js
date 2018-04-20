003569165297362b1e0f2d19cc8c7d09bcf018a9
steven410 <stevenwh2012@gmail.com>
2018-04-19 20:20:38 -0500

    add mLab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"), 
     indexRoutes = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://StevenWH:Quetzacotl1@ds251799.mlab.com:51799/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
