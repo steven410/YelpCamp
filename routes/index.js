6c4f6baa6d9d7d69ef2f4888f0b31347a50077d2
steven410 <stevenwh2012@gmail.com>
2018-04-29 21:24:41 -0500

    refactored sign up and login pages collapsable comment input on show pages

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get("/", function(req, res){
 
 //show register form
 router.get("/register", function(req, res){
-    res.render("register")
+    res.render("register", {page: 'register'})
 });
 
 //handle sign up logic
@@ -17,8 +17,8 @@ router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            req.flash("error", err.message);
-            return res.render("register")
+            console.log(err);
+            return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
             req.flash("success", "Welcome to YelpCamp " + user.username);
@@ -29,7 +29,7 @@ router.post("/register", function(req, res){
 
 //show login form
 router.get("/login", function(req, res){
-    res.render("login");
+    res.render("login", {page: 'login'});
 });
 
 //handling login logic
